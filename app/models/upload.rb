4500b196972eb853b8492906d79b45c0f61c37fe
lien <lienvandensteen@gmail.com>
2015-10-18 14:48:06 +0200

    add a randomized part to the file name (prevent problems with duplicate file names)

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -9,4 +9,13 @@ class Upload < ActiveRecord::Base
 
   geocoded_by :ip_address
   after_validation :geocode
+
+  before_create :randomize_file_name
+
+  private
+
+  def randomize_file_name
+    random_number = SecureRandom.hex(3)
+    self.image_file_name = "#{random_number}-#{self.image_file_name}"
+  end
 end
