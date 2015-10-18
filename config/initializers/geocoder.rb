23d7e4f2e556610decc54eb1cf224056903ceae6
lien <lienvandensteen@gmail.com>
2015-10-18 12:57:30 +0200

    set geocoder settings

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,1 +1,29 @@
-Geocoder::Configuration.timeout = 25
+Geocoder.configure(
+
+  :timeout=>20,
+
+  :lookup=>:yandex,
+
+  :ip_lookup=>:telize,
+
+  :language=>:en,
+
+  :http_headers=>{},
+
+  :use_https=>false,
+
+  :http_proxy=>nil,
+
+  :https_proxy=>nil,
+
+  :api_key=>nil,
+
+  :cache=>nil,
+
+  :cache_prefix=>"geocoder:",
+
+  :units=>:km,
+
+  :distances=>:linear
+
+)
